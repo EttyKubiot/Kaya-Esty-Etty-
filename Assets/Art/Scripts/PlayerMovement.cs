4c5480d9abf0ade02f3aeae2ea4f4404cf35e21b
EttyBlack <6166887@gmail.com>
2021-09-09 13:05:12 +0300

    Try Jump

--- Assets/Art/Scripts/PlayerMovement.cs
+++ Assets/Art/Scripts/PlayerMovement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody rb;
     private Animator animator;
     [SerializeField] private float rotationspeed = 30f;
     [SerializeField] private float movespeed = 150f;
     [SerializeField] private CharacterController controller;
+    public float speedyJump = 250;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponentInChildren<Animator>();
         controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -26,7 +29,9 @@ public class PlayerMovement : MonoBehaviour
        
         float jump = Input.GetAxis("Jump");
         animator.SetFloat("SpeedJump", jump);
-          
+        Vector3 jumpy = new Vector3(0.0f, 3.0f, 0.0f);
+        rb.AddForce(jumpy * speedyJump * 2);
+
     }
 
    
